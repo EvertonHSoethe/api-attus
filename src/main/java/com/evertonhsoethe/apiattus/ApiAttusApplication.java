37de1976e72079ddcbaa9c2f2ca868fe22673cf8
Everton Henrique Soethe <evertonhsoethe@gmail.com>
2026-07-15 22:20:11 -0300

    [Jira Issue] feat: Add new project to git repository

---
+++ src/main/java/com/evertonhsoethe/apiattus/ApiAttusApplication.java
@@ -0,0 +1,13 @@
+package com.evertonhsoethe.apiattus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAttusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAttusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evertonhsoethe/apiattus/ApiAttusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evertonhsoethe.apiattus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAttusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
